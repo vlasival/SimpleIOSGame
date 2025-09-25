97ffb3f200e5414465127875c997b4580aed8cf0
vlasival <vlasival@gmail.com>
2025-09-25 16:14:07 +0700

    Initial Commit

---
+++ SimpleGame/SimpleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleGameApp.swift
+//  SimpleGame
+//
+//  Created by Иван Власенко on 25.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
